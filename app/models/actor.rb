3eb8175ee4f2cbf479b349fd925cd32709328957
rockshellS <spicer.rachelle@gmail.com>
2021-01-02 20:36:14 -0600

    commit tree c7ad7a484f04bd3bd4fd2eeb807cdea90233a855

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,26 @@
 class Actor < ActiveRecord::Base
+    has_many :characters
+    has_many :shows, through: :characters
+
+def full_name
+    "#{self.first_name} #{self.last_name}"
+end
+
+def list_roles
+    characters.collect do |character|
+        "#{character.name} - #{character.show.name}"
+    end
+end
   
 end
+
+# Associate the `Actor` model with the `Character` and `Show` model. An actor
+#   should have many characters and many shows through characters.
+
+# - Write a method in the `Actor` class, `#full_name`, that returns the first and
+#   last name of an actor.
+
+# - Write a method in the `Actor` class, `#list_roles`, that lists all of the
+#   characters that actor has alongside the show that the character is in. So, for
+#   instance, if we had an actor, Peter Dinklage, a character, Tyrion Lannister,
+#   and a show, Game of Thrones, we with
--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,21 @@
 class Character < ActiveRecord::Base
+    belongs_to :actor
+    belongs_to :show
+   
+   
+
+
+    # Define a method in the `Character` class, `#say_that_thing_you_say`, using a
+    # given character's catchphrase. Using Tyrion as an example again, the returned
+    # string should look like the following:
   
+    #   ```ruby
+    #   tyrion.catchphrase = 'A Lannister always pays his debts'
+    #   tyrion.say_that_thing_you_say
+    #   #=> 'Tyrion Lannister always says: A Lannister always pays his debts'
+    #   ```
+  def say_that_thing_you_say
+    "{self.name} always says: #{self.catchphrase}"
+  end
+
 end
